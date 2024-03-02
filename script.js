a630ac8afdf3038508e209d15779c6996358f0e6
sanjana <sanjana.bansal@loblaw.ca>
2024-03-02 17:30:43 -0500

    center the image and spoiler texts spoiler texts are now visible only when block is open

--- script.js
+++ script.js
@@ -12,29 +12,25 @@ function toggleClass(obj, cls){
 var block = document.querySelectorAll('._block');
 var content =  document.querySelector('.content');
 var aud = document.querySelector('audio');
-var playing = false;
+var spoiler = document.querySelectorAll('._spoiler');
 
 for(var i=0; i<block.length; i++){
   block[i].onclick = function(){
     for(var it=0; it<block.length; it++){
       block[it].className = '_block';
+      spoiler[it].style.opacity = 1;
     };
     
     content.className = 'content  __open';
     toggleClass(this, ' __active');
-    // aud.play();
-    playing = true;
     
     document.querySelector('._block.__active').onclick = function(){
       toggleClass(this, ' __active');
       toggleClass(content, ' __open');
-      if(playing){
-        aud.pause();
-        playing = false;
-      } else {
-        aud.play();
-        playing = true;
-      }
+
+      for(var it=0; it<spoiler.length; it++){
+        spoiler[it].style.opacity = 0;
+      };
     }
   }
 }
